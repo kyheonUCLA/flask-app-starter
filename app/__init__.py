25aaa190439d78ada74741593eb3dab0e5d8e2b7
ky heon <kuma@kys-MacBook-Air.local>
2024-02-04 00:39:45 -0800

    allowed CORS data on app

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask, send_from_directory, send_file
+from flask_cors import CORS
 from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__, static_folder='static')
+CORS(app)
 
 from app import routes
